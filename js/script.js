e0f656ca10fcaef7bd5b601779ccb9ed47c2d740
joxasabr05 <jaxongirmirhalikov@gmail.com>
2023-08-17 13:03:33 +0500

    Samar 4-dars JS

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert("Hello Javascript");
+
+// const place = confirm("Are you here?");
+// console.log(place);
+// const currentPlace = prompt("Where are you?", 'Namangan');
+// console.log(currentPlace);
+// console.log(typeof currentPlace);
+
+// const age = +prompt("How ald are you?", "18");
+// console.log(age + 20);
+// console.log(typeof age);
+
+// const favouriteColor = [];
+// favouriteColor[0] = "black";
+// console.log(favouriteColor);
+const favouriteColor = [];
+favouriteColor[0] = prompt("What your favourite color #1");
+favouriteColor[1] = prompt("What your favourite color #2");
+favouriteColor[2] = prompt("What your favourite color #3");
+console.log(favouriteColor);
+console.log(typeof favouriteColor);
